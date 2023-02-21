26739c4740ac6e25c9a04faa7dd2c74124e9acf9
Dovran <d.ovetdiyev@gmail.com>
2023-02-21 21:52:19 +0500

    song pinia

--- src/stores/song.js
+++ src/stores/song.js
@@ -2,7 +2,7 @@ import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 import artist from '../artist.json'
 
-export const useCounterStore=defineStore('song', ()=>{
+export const useSongStore=defineStore('song', ()=>{
     state:()=>({
         isPlaying:false,
         audio:null,
@@ -14,48 +14,59 @@ export const useCounterStore=defineStore('song', ()=>{
         loadSong(artist, track) {
             this.currentArtist=artist
             this.currentTrack=track
+
             if (this.audio && this.audio.src){
                 this.audio.pause()
                 this.isPlaying=false
                 this.audio.src=''
             }
-            this.audio=new Audio()
+            this.audio = new Audio()
             this.audio.src=track.path
             setTimeout(()=>{
                 this.isPlaying=true
                 this.audio.play()
             },200)
         },
-        playOrpauseThisSong(artist.track){
+        playOrPauseSong() {
+            if(this.audio.paused){
+                this.isPlaying=true
+                this.audio.play()
+            }else{
+                this.isPlaying=false
+                this.audio.pause()
+            }
+        },
+        playOrPauseSong(artist,track){
             if(!this.audio || !this.audio.src || (this.currentTrack.id !==track.id)){
                 this.loadSong(artist,track)
                 return
-            }
-            this.playOrpauseSong()
+            } 
+            this.playOrPauseSong()
         },
         prevSong(currentTrack){
-            let track=artist.tracks[currentTrack.id-2]
+            let track=artist.tracks[currentTrack.id - 2]
             this.loadSong(artist,track)
         },
         nextSong(currentTrack){
-            if(currentTrack.id===artists.tracks.length){
+            if(currentTrack.id===artist.tracks.length){
                 let track=artist.tracks[0]
-                this.loadSong(artist,track)
+            this.loadSong(artist,track)
             }else{
                 let track=artist.tracks[currentTrack.id]
                 this.loadSong(artist,track)
             }
         },
-        playFormFirst(){
+        playFromFirst(){
             this.resetState()
             let track=artist.tracks[0]
-            this.loadSong(artist.track)
+            this.loadSong(artist, track)
         },
         resetState(){
             this.isPlaying=false
             this.audio=null
             this.currentArtist=null
             this.currentTrack=null
+
         }
 
     }
